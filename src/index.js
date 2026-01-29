37c8d2331ddb2b0c4940a724e9b6e97ac0287250
LAULANET Lucas <lucas.laulanet@mnt.fr>
2026-01-29 11:24:22 +0100

    app title and escape from keycloak

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import keycloak from './keycloak';
 
-keycloak.init({ onLoad: 'login-required', chekLoginIframe: false, }).then(authentificated => {
-  if(!authentificated) {
-    window.location.reload();
-  }
+//* Initialize Keycloak and then render the app
+//* using double slash to escape from keycloak
+
+//keycloak.init({ onLoad: 'login-required', chekLoginIframe: false, }).then(authentificated => {
+  //if(!authentificated) {
+    //window.location.reload();
+  //}
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>);
-});
+//}); 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+//* If you want to start measuring performance in your app, pass a function
+//* to log results (for example: reportWebVitals(console.log))
+//* or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
