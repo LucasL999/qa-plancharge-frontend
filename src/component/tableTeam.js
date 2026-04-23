acf50446b3ea39722f65a8d24cf2cce470d1c71c
LAULANET Lucas <lucas.laulanet@mnt.fr>
2026-04-23 13:43:07 +0200

    table plan de charge branchée

--- src/component/tableTeam.js
+++ src/component/tableTeam.js
@@ -1,33 +1,51 @@
 import { alpha, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getAllTeam } from "../services/teamService";
+import { getWorkingDaysUntilYearEnd } from "../algo/joursOuvresAn";
 
 export default function TableTeam() {
-    const mockQAs = [
-    { name: "Alice", firstname: "Alice", capacity: 20, absences: 2 },
-    { name: "Bob", firstname: "Bob", capacity: 18, absences: 1 },
-    { name: "Charlie", firstname: "Charlie", capacity: 22, absences: 0 },
-    { name: "David", firstname: "David", capacity: 19, absences: 3 },
-    { name: "Eve", firstname: "Eve", capacity: 21, absences: 1 },
-    { name: "Frank", firstname: "Frank", capacity: 17, absences: 2 },
-    { name: "Grace", firstname: "Grace", capacity: 23, absences: 0 },
-  ];
+    const [QAs, setQAs] = useState([]);
+    useEffect(() => {
+        const fetchQAs = async () => {
+            try {
+                const data = await getAllTeam();
+                setQAs(data);
+            } catch (error) {
+                console.error('Error fetching QAs:', error);
+            }
+        };
+
+        fetchQAs();
+    }, []);
+
+
+    const [workingDays, setWorkingDays] = useState(null);
+    useEffect(() => {
+      async function load(){
+        const result = await getWorkingDaysUntilYearEnd(new Date());
+        setWorkingDays(result);
+      }
+      load();
+    }, []);
+        
   return (
     
 <TableContainer sx={{ boxShadow: 3, borderRadius: 2 }}>
       <Table sx={{ minWidth: 650, borderRadius: 10 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="center" sx={{ fontWeight: "bold", fontSize: "16px" }} >Utilisateur</TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bold", fontSize: "16px" }} >QAs</TableCell>
             <TableCell align="center" sx={{ fontWeight: "bold", fontSize: "16px" }} >Capacité disponible</TableCell>
             <TableCell align="center" sx={{ fontWeight: "bold", fontSize: "16px" }} >Absences restantes</TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {mockQAs.map((qa) => (
+          {QAs.map((qa) => (
             <TableRow key={qa.name} sx={{ ":hover": {backgroundColor: alpha("#5DA1BC", 0.2), cursor: "pointer"} }}>
               <TableCell align="center" sx={{ color: "#0178A5", fontWeight: "bold" }}>{qa.firstname} {qa.name}</TableCell>
-              <TableCell align="center" sx={{color: "#009951", fontWeight: "bold"}}>{qa.capacity}</TableCell>
-              <TableCell align="center" sx={{color: "#6B6B6B", fontWeight: "bold"}}>{qa.absences}</TableCell>
+              <TableCell align="center" sx={{color: "#009951", fontWeight: "bold"}}>{workingDays-qa.nbused}</TableCell>
+              <TableCell align="center" sx={{color: "#6B6B6B", fontWeight: "bold"}}>{qa.nbused}</TableCell>
             </TableRow>
           ))}
         </TableBody>
